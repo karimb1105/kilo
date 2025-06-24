a2a9e9603aeee1188054a3081accf7e589185cb0
karimb1105 <samuelkarim.benhamou@gmail.com>
2025-06-24 13:35:08 -0500

    solved third problem

--- basics/project-euler.c
+++ basics/project-euler.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+int euler1();
+int euler2();
+bool isPrime();
 
 // Find the sum of all multiples of 3 or 5 below 1000
 int euler1() {
@@ -29,10 +34,36 @@ int euler2() {
     return sum;
 }
 
+bool isPrime(int num) {
+    for(int i = 2; i * i <= num; i++) {
+        if(num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// What is the largest prime factor of the number 600851475143?
+int euler3() {
+    long target = 600851475143;
+    long factor = 2;
+    long largestPrime = 0;
+    while(target > 1) {
+        if(target % factor == 0) {
+            if(isPrime(factor)) {
+                largestPrime = factor;
+                while(target % factor == 0) {
+                    target /= factor;
+                }
+            }
+        }
+        factor++;
+    }
+    return largestPrime;
+}
+
 int main(void) {
-  printf("meow\n");
-  int result1 = euler1();
-  int result2 = euler2();
-  printf("%d\n", result2);
+  int result = euler3();
+  printf("%d\n", result);
   return 0;
 }
